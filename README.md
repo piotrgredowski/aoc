127bdd212cefe89e86166f9a5d0887bb79e68281
Piotr Grędowski <piotrgredowski@gmail.com>
2024-12-01 19:22:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of Code
+
+## Development
+
+To install the dependencies, run
+
+
+```bash
+uv pip install -e ".[dev]"
+```
+
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from advent_of_code.main import get_greeting
+
+
+def test_get_greeting():
+    assert get_greeting() == "Hello, world!"
